0dbd8f0976bb903700ff0d2ebc472663f14e9c35
arunpranav-at <pranav9176@gmail.com>
2024-05-07 23:23:43 +0530

    added assignment 4

--- Assignment4_0705/normaldistributionvisualization.py
+++ Assignment4_0705/normaldistributionvisualization.py
@@ -7,19 +7,19 @@ import math
 def fun(x, u, s):
     return (math.exp(-0.5 * (((x - u) / s)**2))) / (s * math.sqrt(2 * math.pi)) #normal distribution function
 
-ulst = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5] #mean values
-slst = [0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4] #standard deviation values
-
+ulst = [-4, -3, -2, -1, 0, 1, 2, 3, 4] #mean values
+slst = [0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5] #standard deviation values
+colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'pink', 'orange']
 u = 0
 fig = plt.figure(figsize =(8, 8))
 for s in slst:
     x = np.arange(-10, 10, 0.001)
     y = np.vectorize(fun)(x, u, s)
-    plt.plot(x, y, label = 'Normal Distribution with same Mean and SD ', color = 'blue')
-plt.title('Normal Distribution with same Mean and different Standard Deviation values')
-plt.xlabel('X values (Mean)')
+    plt.plot(x, y, label = f'Normal Distribution with \u03c3 value {s}', color = colors[slst.index(s)])
+plt.title('Normal Distribution with same Mean \u03bc = 0 and different Standard Deviation values')
+plt.xlabel('\u03bc values (Mean)')
 plt.ylabel('Y values (Probability Density Function)')
-plt.legend(['Normal Distribution Curves'], title='Legend')
+plt.legend(title='Legend')
 plt.figtext(0.5, 0.01, 'We are plotting normal distribution curve for same mean and different standard deviation values. We observe that the position of peak doesnot change as it is dependent on Mean and height and width of peak changes as it is dependent on Standard Deviation', wrap=True, horizontalalignment='center', fontsize=12)
 
 s = 1
@@ -27,12 +27,13 @@ fig = plt.figure(figsize =(8, 8))
 for u in ulst:
     x = np.arange(-10, 10, 0.001)
     y = np.vectorize(fun)(x, u, s)
-    plt.plot(x, y, label = 'Normal Distribution with same Standard Deviation', color = 'blue')
-plt.title('Normal Distribution with same Standard Deviation and different Mean values')
-plt.xlabel('X values (Mean)')
+    plt.plot(x, y, label = f'Normal Distribution with \u03bc value {u}', color = colors[ulst.index(u)])
+plt.title('Normal Distribution with same Standard Deviation \u03c3 = 1 and different Mean values')
+plt.xlabel('\u03bc values (Mean)')
 plt.ylabel('Y values (Probability Density Function)')
-plt.legend(['Normal Distribution Curves'], title='Legend')
-plt.figtext(0.5, 0.01, 'We are plotting normal distribution curve for same mean and different standard deviation values. We observe that the height and width of peak doesnot change as it is dependent on Standard Deviation and position of peak changes as it is dependent on Mean', wrap=True, horizontalalignment='center', fontsize=12)
+plt.xticks(np.arange(-10, 10, 1))
+plt.legend(title='Legend')
+plt.figtext(0.5, 0.01, 'We are plotting normal distribution curve for same standard deviation and different mean values. We observe that the height and width of peak doesnot change as it is dependent on Standard Deviation and position of peak changes as it is dependent on Mean', wrap=True, horizontalalignment='center', fontsize=12)
 plt.show()
     
             
--- README.md
+++ README.md
@@ -104,3 +104,17 @@ For functionalrandomsampler.py, output generated one time was:
 ```
 and randomsampler.py generated output once like this:
 ![assignment3](<Pictures/assignment3.png>)<br>
+
+## Assignment 4 - 07/05/2024
+Problem Statement: Plot graph following seven principles of graph plotting for same mean with different standard deviation values (Graph 1) and same standard deviation with different mean values (Graph 2)
+
+Code: Refer Assigment4_0705/normaldistributionvisualization.py
+```
+pip install matplotlib, mpl_toolkits, numpy, math, random
+cd Assigment4_0705
+python3 normaldistributionvisualization.py
+```
+
+Output: 
+![Same Mean](Pictures/samemeanassign4.png)
+![Same SD](Pictures/samesdassign4.png)
